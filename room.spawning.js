3960927ab41b9d374479cb833242c5551fbcd668
Melnikov Artem <melnikovam10@gmail.com>
2021-07-30 23:05:23 +0300

    some function for future auto

--- room.spawning.js
+++ room.spawning.js
@@ -1,23 +1,10 @@
 require('constants')
 
-var target = {};
-target[HARVESTERS_ROLENAME] = HARVESTERS_DESIRED;
-target[BUILDERS_ROLENAME]   = BUILDERS_DESIRED;
-target[UPGRADERS_ROLENAME]  = UPGRADERS_DESIRED;
-
-
-var target_identity = {};
-target_identity[HARVESTERS_ROLENAME] = [BUMBLEBEE_PARTS, BUMBLEBEE_TYPENAME];
-target_identity[BUILDERS_ROLENAME]   = [BUMBLEBEE_PARTS, BUMBLEBEE_TYPENAME];
-target_identity[UPGRADERS_ROLENAME]  = [BUMBLEBEE_PARTS, BUMBLEBEE_TYPENAME];
-
-var emergency_roles = [HARVESTERS_ROLENAME] //["harvester"];
-
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function spawnCreepInMainRom(role, weak = 0) {
+function spawnCreepInMainRom(role, room, weak = 0) {
   var parts = target_identity[role][0];
   var type =  target_identity[role][1];
   var creep_name = type + '' + getRandomInt(10000);
@@ -27,27 +14,46 @@ function spawnCreepInMainRom(role, weak = 0) {
     parts = WEAK_PARTS;
   }
 
-
-  return Game.spawns[SPAWN_NAME].spawnCreep(parts,  creep_name, { memory: { role: role, type: type, switched: Game.time } } );
+  return Game.spawns[SPAWN_NAME].spawnCreep(parts,  creep_name, { memory: { role: role, type: type, switched: Game.time, homeroom: room.name } } );
 }
 
-function count_roles(role) {
-  return _.filter(Game.creeps, (creep) => creep.memory.role == role).length;
+function get_target() {
+  let target = {};
+  target[HARVESTERS_ROLENAME] = HARVESTERS_DESIRED;
+  target[BUILDERS_ROLENAME]   = BUILDERS_DESIRED;
+  target[UPGRADERS_ROLENAME]  = UPGRADERS_DESIRED;
+  return target;
 }
 
+var target_identity = {};
+target_identity[HARVESTERS_ROLENAME] = [BUMBLEBEE_PARTS, BUMBLEBEE_TYPENAME];
+target_identity[BUILDERS_ROLENAME]   = [BUMBLEBEE_PARTS, BUMBLEBEE_TYPENAME];
+target_identity[UPGRADERS_ROLENAME]  = [BUMBLEBEE_PARTS, BUMBLEBEE_TYPENAME];
+
+function get_real(room) {
+  let real = {};
+  real[HARVESTERS_ROLENAME] = _.filter(Game.creeps, (creep) => creep.memory.role == HARVESTERS_ROLENAME && creep.memory.homeroom == room.name).length;
+  real[BUILDERS_ROLENAME]   = _.filter(Game.creeps, (creep) => creep.memory.role == BUILDERS_ROLENAME   && creep.memory.homeroom == room.name).length;
+  real[UPGRADERS_ROLENAME]  = _.filter(Game.creeps, (creep) => creep.memory.role == UPGRADERS_ROLENAME  && creep.memory.homeroom == room.name).length;
+
+  return real;
+}
 
 function roomSpawning(room) {
-    var real = {};
-    real[HARVESTERS_ROLENAME] = count_roles(HARVESTERS_ROLENAME);
-    real[BUILDERS_ROLENAME]   = count_roles(BUILDERS_ROLENAME);
-    real[UPGRADERS_ROLENAME]  = count_roles(UPGRADERS_ROLENAME);
+    let real   = get_real(room);
+    let target = get_target(room);
 
+
+    if (Game.time % 200 == 0) {
+      console.log("Room " + room.name + ":");
+    }
 
     for (var role in target) {
-      if (Game.time % 100 == 0)
+      if (Game.time % 200 == 0) {
         console.log(role + ": " + real[role] + "/" + target[role]);
+      }
       if (real[role] < target[role]) {
-        if (spawnCreepInMainRom(role) == OK) {
+        if (spawnCreepInMainRom(role, room) == OK) {
           console.log('spawned ' + role);
           break;
         }
