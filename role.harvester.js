8f45f29714aa79f0c37dc096c5c648d326d7e1f5
Melnikov Artem <melnikovam10@gmail.com>
2021-07-30 18:50:36 +0300

    fflush tag for harvester

--- role.harvester.js
+++ role.harvester.js
@@ -4,32 +4,37 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.store.getFreeCapacity() > 0) {
+      if (creep.store[RESOURCE_ENERGY] == 0) {
+        creep.memory.fflush = false;
+      }
+	    if(creep.store.getFreeCapacity() > 0 && !creep.memory.fflush) {
             roleFunctions.harvestClosesSource(creep);
         }
-
-        else {
-          var target = null;
-          if (Game.rooms['E21S57'].energyAvailable < 550) {
-            target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN) &&
-                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-            });
-          }
-          if (!target) {
-            target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER ||
-                                structure.structureType == STRUCTURE_CONTAINER) &&
-                                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-            });
-          }
+      else if (creep.store.getFreeCapacity() == 0 && !creep.memory.fflush){
+        creep.memory.fflush = true;
+      }
+      else {
+        var target = null;
+        if (Game.rooms['E21S57'].energyAvailable < 550) {
+          target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                  filter: (structure) => {
+                      return (structure.structureType == STRUCTURE_EXTENSION ||
+                              structure.structureType == STRUCTURE_SPAWN) &&
+                              structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                  }
+          });
+        }
+        if (!target) {
+          target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                  filter: (structure) => {
+                      return (structure.structureType == STRUCTURE_EXTENSION ||
+                              structure.structureType == STRUCTURE_SPAWN ||
+                              structure.structureType == STRUCTURE_TOWER ||
+                              structure.structureType == STRUCTURE_CONTAINER) &&
+                              structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                  }
+          });
+        }
 
           if (target) {
               if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
@@ -37,6 +42,9 @@ var roleHarvester = {
               }
           } else {
             creep.moveTo(Game.spawns['Spawn1']);
+            if (creep.store.getFreeCapacity() > 0) {
+              creep.memory.fflush = false;
+            }
           }
         }
 	}
