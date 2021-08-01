ef2db716c3bc105f03e4521b30051638e0fcaf78
Melnikov Artem <melnikovam10@gmail.com>
2021-08-01 03:45:36 +0300

    armies file with auto orders and knight role

---
+++ armies.js
@@ -0,0 +1,123 @@
+function checkRoom(room) {
+  let enemy_creeps = room.find(FIND_HOSTILE_CREEPS);
+
+  let enemy_structures = room.find(FIND_HOSTILE_STRUCTURES, {
+    filter: (structure) => structure.structureType == STRUCTURE_TOWER ||
+      structure.structureType == STRUCTURE_INVADER_CORE
+  });
+
+  let enemies = {
+    ...enemy_structures,
+    ...enemy_creeps
+  }
+
+  if (!enemies) {
+    enemies = room.find(FIND_HOSTILE_STRUCTURES, {
+      filter: (structure) => structure.structureType == STRUCTURE_SPAWN ||
+        structure.structureType == STRUCTURE_POWER_SPAWN
+    });
+  }
+
+  return enemies;
+}
+
+
+function checkSafeRoom(army) {
+  let enemy_creeps = room.find(FIND_HOSTILE_CREEPS);
+
+  let enemy_structures = room.find(FIND_HOSTILE_STRUCTURES, {
+    filter: (structure) => structure.structureType == STRUCTURE_RAMPART ||
+      structure.structureType == STRUCTURE_EXTENSION
+  });
+
+  let enemies = {
+    ...enemy_structures,
+    ...enemy_creeps
+  }
+
+  return enemies;
+}
+
+
+function armyLoop() {
+  for (let armyName in Memory.armies) {
+    //right now more like a mob that comes in room
+    let army = Memory.armies[armyName];
+    let stationedRoom = Game.rooms[army.stationed];
+
+    let enemies;
+    //check room depending on situation
+    if (army.high_alert) {
+      enemies = checkRoom(stationedRoom);
+    } else if (stationedRoom.controller.my && Game.time % 100 == 0) {
+      enemies = checkRoom(stationedRoom);
+    } else if (Game.time % 50 == 0) {
+      enemies = checkRoom(stationedRoom);
+    }
+
+    let soliders = _.filter(Game.creeps, (creep) => creep.memory.army_name == armyName);
+
+    if (stationedRoom.controller.my) {
+      if (army.replenished == 0 || Game.time % 50 == 0) {
+        let armyFull = 1;
+        // if needed the room will start replenishing
+        _.forEach(Object.keys(army.roles), function(roleName) {
+          let diff = army.roles[roleName] - _.filter(soliders, (creep) => creep.memory.role == roleName).length
+          if (diff > 0) {
+            armyFull = 0;
+          }
+
+          let inProcess = _.get(stationedRoom.memory, ["army_orders", roleName]);
+          if (inProcess) {
+            diff -= inProcess.count;
+          }
+
+          if (diff > 0) {
+            // those in process not enough
+            if (!stationedRoom.memory.army_orders) {
+              stationedRoom.memory.army_orders = {};
+            }
+            stationedRoom.memory.army_orders[roleName] = {
+              armyName: armyName,
+              count: diff
+            };
+            console.log("New " + armyName + " order for " + diff + " " + roleName);
+          }
+        });
+        if (armyFull) {
+          army.replenished = 1;
+        } else {
+          army.replenished = 0;
+        }
+      }
+    }
+
+    if (soliders.length == 0 && !stationedRoom.controller.my) {
+      delete Memory.armies[armyName];
+    } else if (enemies) {
+      // time to fight enemies should be defined
+      army.high_alert = 1;
+      army.reuse_path = 0;
+
+    } else {
+      army.high_alert = 0;
+      army.reuse_path = 10;
+
+      if (army.stationed != army.target && army.replenished) {
+        // move to next room
+      } else {
+        let enemies = checkSafeRoom(stationedRoom);
+        if (enemies) {
+          // pillage?
+
+        } else {
+          //chose a spot and chill?
+
+        }
+      }
+    }
+  }
+}
+
+
+module.exports = armyLoop;
---
+++ role.knight.js
@@ -0,0 +1,83 @@
+let roleKnight = {
+  run: function(creep) {
+    let orders = creep.memory.orders;
+    if (orders.move) {
+      creep.moveTo(new RoomPosition(orders.move.x, orders.move.y, creep.roomName));
+    }
+    if (creep.memory.orders.attack) {
+      creep.attack(Game.getObjectById(creep.memory.orders.attack));
+    }
+  },
+
+  coolName: "Fire ant ",
+  spawn: function(room) {
+    let roleName = "knight";
+    let orderInfo = _.get(room.memory, ["army_orders", roleName]);
+
+    if (Game.time % OUTPUT_TICK == 0) {
+      let status = "no order"
+      if (orderInfo) {
+        status = "ordered " + orderInfo.count;
+      }
+      console.log("knights: " + status);
+    }
+
+
+    if (!orderInfo) {
+      return
+    }
+
+    let spawnSettings = {
+      bodyParts: [],
+      memory: {}
+    }
+    let roomEnergy = room.energyCapacityAvailable;
+
+    let segment = [TOUGH, ATTACK, MOVE];
+    let segmentCost = _.sum(segment, s => BODYPART_COST[s]);
+    let sumCost = segmentCost;
+
+    for (let i = 0; sumCost + segmentCost <= roomEnergy &&
+      spawnSettings.bodyParts.length + segment.length <= 50; ++i) {
+      _.forEach(segment, (s) => spawnSettings.bodyParts.push(s));
+      sumCost += segmentCost;
+
+      if (i % 2 == 0) {
+        segment = [ATTACK, ATTACK, MOVE];
+      } else if (i % 4 == 1) {
+        segment = [TOUGH, ATTACK, MOVE];
+      } else if (i % 4 == 3) {
+        segment = [TOUGH, TOUGH, MOVE];
+      }
+      segmentCost = _.sum(segment, s => BODYPART_COST[s]);
+    }
+
+    segment = [TOUGH, TOUGH, MOVE];
+    if (sumCost + segmentCost <= roomEnergy &&
+      spawnSettings.bodyParts.length + segment.length <= 50) {
+      _.forEach(segment, (s) => spawnSettings.bodyParts.push(s));
+      sumCost += segmentCost;
+    }
+
+    spawnSettings.memory = {
+      role: roleName,
+      born: Game.time,
+      army_name: orderInfo.armyName,
+      orders: {
+        attack: 0,
+        move: 0
+      }
+    };
+
+    spawnSettings.postSpawn = function() {
+      room.memory.army_orders[roleName].count -= 1
+      if (room.memory.army_orders[roleName].count <= 0) {
+        delete room.memory.army_orders[roleName];
+      }
+    };
+
+    return spawnSettings;
+  },
+}
+
+module.exports = roleKnight;
