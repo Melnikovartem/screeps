2a87156d448a8f79a1b7251e958e59b4c6bc7cdc
Melnikov Artem <melnikovam10@gmail.com>
2021-08-14 20:11:59 +0300

    i am ready for everything to fall, but i am going to test on public cause no minerals(need 2 different) in testing world

--- src/cells/base/laboratoryCell.ts
+++ src/cells/base/laboratoryCell.ts
@@ -1,6 +1,8 @@
 import { Bee } from "../../bee";
 import { Cell } from "../_Cell";
 import { Hive } from "../../Hive";
+
+import { makeId } from "../../utils";
 import { profile } from "../../profiler/decorator";
 
 /*
@@ -20,22 +22,23 @@ console.log(ss);
 */
 
 type BaseMineral = "H" | "O" | "Z" | "L" | "K" | "U"
-type ResourceLab = "G" | "OH" | "X" | "ZK" | "UL" | "LH" | "ZH" | "GH" | "KH" | "UH" | "LO" | "ZO" | "KO" | "UO" | "GO" | "LH2O" | "KH2O" | "ZH2O" | "UH2O" | "GH2O" | "LHO2" | "UHO2" | "KHO2" | "ZHO2" | "GHO2" | "XUH2O" | "XUHO2" | "XKH2O" | "XKHO2" | "XLH2O" | "XLHO2" | "XZH2O" | "XZHO2" | "XGH2O" | "XGHO2";
+type ReactionConstant = "G" | "OH" | "X" | "ZK" | "UL" | "LH" | "ZH" | "GH" | "KH" | "UH" | "LO" | "ZO" | "KO" | "UO" | "GO" | "LH2O" | "KH2O" | "ZH2O" | "UH2O" | "GH2O" | "LHO2" | "UHO2" | "KHO2" | "ZHO2" | "GHO2" | "XUH2O" | "XUHO2" | "XKH2O" | "XKHO2" | "XLH2O" | "XLHO2" | "XZH2O" | "XZHO2" | "XGH2O" | "XGHO2";
 
-const reactionMap: { [key in ResourceLab]?: { res1: BaseMineral | ResourceLab, res2: BaseMineral | ResourceLab } } = {};
+const reactionMap: { [key in ReactionConstant]?: { res1: BaseMineral | ReactionConstant, res2: BaseMineral | ReactionConstant } } = {};
 for (let res1 in REACTIONS) {
   for (let res2 in REACTIONS[res1])
-    reactionMap[<ResourceLab>REACTIONS[res1][res2]] = {
-      res1: <BaseMineral | ResourceLab>res1,
-      res2: <BaseMineral | ResourceLab>res2,
+    reactionMap[<ReactionConstant>REACTIONS[res1][res2]] = {
+      res1: <BaseMineral | ReactionConstant>res1,
+      res2: <BaseMineral | ReactionConstant>res2,
     };
 }
+const baseMinerals = ["H", "O", "Z", "L", "K", "U"];
 //reaction map done
 
 interface SynthesizeRequest {
-  amount: number,
   start: number,
-  resource: ResourceLab,
+  amount: number,
+  resource: ReactionConstant,
 };
 
 @profile
@@ -44,19 +47,63 @@ export class laboratoryCell extends Cell {
   // inLab id - ResourceConstant
   inLab: { [id: string]: number } = {};
   boostRequests: { bee: Bee, amount: number, resource: ResourceConstant }[] = [];
-  synthesizeRequests: SynthesizeRequest[] = [];
+  synthesizeRequests: { [id: string]: SynthesizeRequest } = {};
 
   constructor(hive: Hive) {
     super(hive, "LaboratoryCell_" + hive.room.name);
 
-    for (let reaction in REACTION_TIME)
+    for (let reaction in reactionMap)
       this.inLab[reaction] = 0;
+    for (let mineral in baseMinerals)
+      this.inLab[mineral] = 0;
+  }
+
+  newsynthesizeRequest(resource: ReactionConstant, amount?: number) {
+    if (!amount) {
+      amount = 0;
+      let mainStore = this.hive.cells.storage && this.hive.cells.storage.storage.store;
+      if (mainStore) {
+        let res1 = reactionMap[resource]!.res1;
+        let res2 = reactionMap[resource]!.res2;
+
+        amount = Math.min(mainStore[res1], mainStore[res2]);
+      }
+    }
+    if (amount > 0)
+      this.synthesizeRequests[resource + "_" + amount + "_" + makeId(6)] = {
+        start: amount,
+        amount: amount,
+        resource: resource,
+      };
+  }
+
+  toStorage(res: ReactionConstant, amount: number, ref?: string) {
+    let storageCell = this.hive.cells.storage;
+    if (storageCell) {
+      storageCell.requests[ref ? ref : makeId(6)] = {
+        to: [storageCell.storage],
+        from: this.laboratories,
+        resource: res,
+        amount: amount,
+        priority: 3,
+      }
+      this.inLab[res] = amount ? this.inLab[res] - amount : 0;
+    }
   }
 
   update() {
     super.update();
 
     if (this.laboratories.length) {
+      if (Game.time % 100 == 29 || this.time == Game.time)
+        _.forEach(this.laboratories, (lab) => {
+          for (let res in lab.store) {
+            let inStore = lab.store[<BaseMineral | ReactionConstant>res];
+            if (inStore && this.inLab[res] < inStore)
+              this.inLab[res] = inStore;
+          }
+        });
+
       let storageCell = this.hive.cells.storage;
       if (storageCell) {
         let storageRequests: { [id: string]: number } = {};
@@ -89,7 +136,7 @@ export class laboratoryCell extends Cell {
         // TODO: boosting
 
         for (let res in storageRequests) {
-          let resource: ResourceLab = <ResourceLab>res;
+          let resource: ReactionConstant = <ReactionConstant>res;
           while (storageRequests[resource] > 0) {
             let amount = Math.min(storageRequests[resource], LAB_MINERAL_CAPACITY);
             storageRequests[resource] -= LAB_MINERAL_CAPACITY;
@@ -135,11 +182,11 @@ export class laboratoryCell extends Cell {
               request.amount -= 5;
             }
           }
-        } else {
-          let storageCell = this.hive.cells.storage;
-          if (storageCell) {
-          }
-        }
+        } else if (request.start > 0) {
+          request.start = 0;
+          this.toStorage(request.resource, request.start, key);
+        } else
+          delete this.synthesizeRequests[key];
       }
     }
   }
