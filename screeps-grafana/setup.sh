7a2fc1b60e82f83d54fa718bf2fbae0d48955c5b
Artem Melnikov <melnikovam10@gmail.com>
2023-08-20 14:58:09 +0300

    grafana fixed

--- screeps-grafana/setup.sh
+++ screeps-grafana/setup.sh
@@ -12,7 +12,7 @@ echo Configuring Datasource...
 curl -s 'http://admin:admin@localhost:1337/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name":"localGraphite","type":"graphite","url":"http://graphite:8000","access":"proxy","isDefault":true,"database":""}' > /dev/null
 sleep 2
 echo Installing Sample...
-curl -s 'http://admin:admin@localhost:1337/api/dashboards/db' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data @sampleDashboard.json  > /dev/null
+curl -s 'http://admin:admin@localhost:1337/api/dashboards/db' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data @dashboard.json  > /dev/null
 echo All done! 
 echo You should be able connect to http://localhost:1337
 echo with username \'admin\' and password \'admin\'
